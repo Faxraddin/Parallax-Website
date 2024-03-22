4857adfa8c54e5ff0d5391ca9eff83dd630fc44c
Faxraddin <reydivonay@gmail.com>
2024-03-22 19:27:38 +0400

    z moves

--- js/app.js
+++ js/app.js
@@ -1,14 +1,30 @@
 const parallax_el = document.querySelectorAll(".parallax");
 
 let xValue = 0, yValue = 0;
+let rotateDegree = 0;
 
 window.addEventListener('mousemove',(e)=>{
     xValue=e.clientX - window.innerWidth/2;
     yValue=e.clientY - window.innerHeight/2;
 
+    rotateDegree = (xValue / (window.innerWidth / 2)) * 20;
+
     parallax_el.forEach(el=>{
         let speedx = el.dataset.speedx;
         let speedy = el.dataset.speedy;
-        el.style.transform = `translate(calc(-50% + ${-xValue * speedx}px)) translateY(calc(-50% + ${yValue * speedy}px))`;
+        let speedz = el.dataset.speedz;
+        let rotateSpeed = el.dataset.rotation;
+
+        let isInLeft =
+          parseFloat(getComputedStyle(el).left) < window.innerWidth / 2
+            ? 1
+            : -1;
+        let zValue = e.clientX - parseFloat(getComputedStyle(el).left) * isInLeft * 0.1;
+
+        el.style.transform = `rotateY(${rotateDegree * rotateSpeed}deg) translate(calc(-50% + ${
+          -xValue * speedx
+        }px)) translateY(calc(-50% + ${
+          yValue * speedy
+        }px)) perspective(2300px) translateZ(${zValue * speedz}px`;
     })
 })
