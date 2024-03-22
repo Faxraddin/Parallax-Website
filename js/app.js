05be7beeeca2772d92d0b13e5b33e43cc3dc34b4
Faxraddin <reydivonay@gmail.com>
2024-03-22 18:45:22 +0400

    horizontal moves

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ window.addEventListener('mousemove',(e)=>{
     yValue=e.clientY - window.innerHeight/2;
 
     parallax_el.forEach(el=>{
-        el.style.transform = `translate(calc(-50% + ${-xValue}px)) translateY(calc(-50% + ${yValue}px))`;
+        let speedx = el.dataset.speedx;
+        el.style.transform = `translate(calc(-50% + ${-xValue * speedx}px)) translateY(calc(-50% + ${yValue}px))`;
     })
 })
